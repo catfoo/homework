57661977be53d9e826e0fd036e890d9d0d8e5f64
catfoo <catfoo@users.noreply.github.com>
2015-06-21 12:27:13 -0400

    Moderate catfood enthusiasm

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, world! I LOVE catfood!");
+    System.out.println("Hello, world! I love catfood!");
   }
 }
